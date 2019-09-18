36f480d0c5e93e604b9a17cd85e9b79de1f8d522
Pratap <dhal.pratapbhanu@gmail.com>
2019-09-18 13:02:38 +0530

    Initial Commit

---
+++ loginattempt/src/main/java/com/loginattempt/user/dao/UserDetailsDao.java
@@ -0,0 +1,13 @@
+package com.loginattempt.user.dao;
+
+import com.loginattempt.user.model.UserAttempts;
+
+public interface UserDetailsDao {
+
+	void updateFailAttempts(String username);
+
+	void resetFailAttempts(String username);
+	
+	UserAttempts getUserAttempts(String username);
+
+}
